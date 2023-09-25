a3cd67c44bcb9e450b248ee3bfb3119629ff44e7
Shivank singh <shivanksingh634@gmail.com>
2023-09-25 08:34:30 +0530

    added Header and hero component

---
+++ src/Common/Layout.js
@@ -0,0 +1,19 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const Layout = ({sx={}, ...props}) => {
+    const css = {
+      maxWidth:"1000px",
+      padding:"10px",
+      TextAlign:"center",
+      margin:"0 auto",
+      ...sx
+    }
+    return (
+    <>
+      <Stack sx={css}  {...props} />
+    </>
+  )
+}
+
+export default Layout
